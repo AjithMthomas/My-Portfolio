c9d10d82c22eae81e8ab687e89462da1fd691a22
AjithMthomas <ajithmt46958@gmail.com>
2024-06-06 23:08:37 +0530

    first commit

---
+++ myPortfolio/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'/home'
+    },
+    {
+        path:'home',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'',
+                component:HomeComponent
+            },
+            {
+                path:'test',
+                component:TestComponent
+            }
+        ]
+    }
+];
